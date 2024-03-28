20e742a9b9ddadbad69df1b47e1821dee4b7f75f
ariel <arielshongoordi@gmail.com>
2024-03-28 19:38:36 +0200

    first commit

---
+++ resources/js/components/Example.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Example() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Ariel est la </div>
+
+                        <div className="card-body">tu nes pas la </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById('example')) {
+    const Index = ReactDOM.createRoot(document.getElementById("example"));
+
+    Index.render(
+        <React.StrictMode>
+            <Example/>
+        </React.StrictMode>
+    )
+}
---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+// webpack.mix.js
+
+const mix = require('laravel-mix');
+
+mix.react('resources/js/app.js', 'public/js')
+   .postCss('resources/css/app.css', 'public/css', [
+      //
+   ]);
